971efa4a288fd734e1126ba13c3341f0718c9ea0
David Dvergsten <ddvergsten@gmail.com>
2020-12-02 07:55:15 -0600

    add scaling to matrix

--- 04-3d-transforms/projects/DgdChapter4/DgdChapter4/Renderer.swift
+++ 04-3d-transforms/projects/DgdChapter4/DgdChapter4/Renderer.swift
@@ -19,7 +19,7 @@ class Renderer: NSObject {
         Renderer.device = device
         Renderer.commandQueue = commandQueue
         metalView.device = device
-        let allocator = MTKMeshBufferAllocator(device: device)
+        //let allocator = MTKMeshBufferAllocator(device: device)
         let vertexDescriptor2 = MTLVertexDescriptor()
         vertexDescriptor2.attributes[0].format = .float3
         vertexDescriptor2.attributes[0].offset = 0
@@ -71,7 +71,7 @@ class Renderer: NSObject {
 //            return [vertex.x, vertex.y, vertex.z]
 //        }
         //vertices[0] += [0.3, -0.4, 0.5]
-        transformedBuffer = device.makeBuffer(bytes: &vertices, length: MemoryLayout<float3>.stride * vertices.count, options: [])
+        //transformedBuffer = device.makeBuffer(bytes: &vertices, length: MemoryLayout<float3>.stride * //vertices.count, options: [])
         
         
         
@@ -82,7 +82,11 @@ class Renderer: NSObject {
     }
     var originalBuffer: MTLBuffer!
     var transformedBuffer: MTLBuffer!
-    var vertices:[float3] = [float3(0.0, 0.0, 0.5)]
+    var vertices:[float3] = [
+        [-0.7, 0.8, 1],
+        [-0.7, -0.4, 1],
+        [0.4, 0.2, 1]
+    ]
 }
 
 extension Renderer: MTKViewDelegate{
@@ -98,10 +102,18 @@ extension Renderer: MTKViewDelegate{
         }
         
         var matrix1 = matrix_identity_float4x4
-        matrix1.columns.3 = [-0.3, 0.4, 0.0, 1.0]
+        //matrix1.columns.3 = [0.0, 0.0, 0.0, 1.0]
+        let scaleX: Float = 1.2
+        let scaleY: Float = 0.5
+        matrix1 = float4x4(
+            [scaleX, 0, 0, 0],
+            [0, scaleY, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        )
         
-        var matrix2 = matrix_identity_float4x4
-        matrix2.columns.3 = [0.3, -0.4, 0, 1]
+//        var matrix2 = matrix_identity_float4x4
+//        matrix2.columns.3 = [0.3, -0.4, 0, 1]
         
         //we setup the pipeline state in init, telling us what shaders to use and
         //and vertex descriptor format for the vertex shader
@@ -114,13 +126,13 @@ extension Renderer: MTKViewDelegate{
         renderEncoder.setFragmentBytes(&greenColor, length: MemoryLayout<float4>.stride, index: 0)
         
         //draw the point at index 0 and count = 1 at this point because we have an array of float3's
-        renderEncoder.drawPrimitives(type: .point, vertexStart: 0, vertexCount: vertices.count)
+        renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertices.count)
         
-        renderEncoder.setVertexBytes(&matrix2, length: MemoryLayout<float4x4>.stride, index: 1)
-        renderEncoder.setVertexBuffer(transformedBuffer, offset: 0, index: 0)
-        var redColor:float4 = float4(1.0, 0.0, 0.0, 1.0)
-        renderEncoder.setFragmentBytes(&redColor, length: MemoryLayout<float4>.stride, index: 0)
-        renderEncoder.drawPrimitives(type: .point, vertexStart: 0, vertexCount: vertices.count)
+//        renderEncoder.setVertexBytes(&matrix2, length: MemoryLayout<float4x4>.stride, index: 1)
+//        renderEncoder.setVertexBuffer(transformedBuffer, offset: 0, index: 0)
+//        var redColor:float4 = float4(1.0, 0.0, 0.0, 1.0)
+//        renderEncoder.setFragmentBytes(&redColor, length: MemoryLayout<float4>.stride, index: 0)
+//        renderEncoder.drawPrimitives(type: .point, vertexStart: 0, vertexCount: vertices.count)
         //we're done drawing with this encoder, can we do more draw commands with this single renderEncoder??
         renderEncoder.endEncoding()
         guard let drawable = view.currentDrawable else {
